392cb66fc94688e099c15609d835db14316b116f
M Khmar Shaikh <shahid13384@hotmail.com>
2020-05-29 18:29:34 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,125 @@
+## What is tzlib ?
+    It provides extension methods on javascript Array. 
+    Some methods are replica of linq in .Net (C#).
+## Function Documentation :
+ ### *first()*
+___
+> Returns first elements of an array.
+```javascript
+function first() {
+    let numArray = new Array<number>();
+        numArray.push(3);
+        numArray.push(100);
+        numArray.push(2);
+        numArray.push(10);
+        numArray.push(4);
+  console.log(numArray.first());  // This will print 3
+
+}
+```
+ ### *last()*
+___
+> Returns last elements of an array.
+```javascript
+function last() {
+    let numArray = new Array<number>();
+        numArray.push(3);
+        numArray.push(100);
+        numArray.push(2);
+        numArray.push(10);
+        numArray.push(4);
+  console.log(numArray.last());  // This will print 4
+
+}
+```
+ ### *where(predicate)*
+___
+> Returns subset of source Array that satisfies predicate.
+```javascript
+function where() {
+    let numArray = new Array<number>();
+        numArray.push(3);
+        numArray.push(100);
+        numArray.push(2);
+        numArray.push(10);
+        numArray.push(4);
+  console.log(numArray.where(ele => ele > 10));  // This will print 100
+  console.log(numArray.where(ele => ele === 3)); // This will print 3
+
+}
+```
+ ### *addRange()*
+___
+> Returns Array which is superset of two arrays.
+```javascript
+function addRange() {
+    let numArray = new Array<number>();
+        numArray.push(3);
+        numArray.push(100);
+        numArray.push(2);
+        numArray.push(10);
+        numArray.push(4);
+
+    const augArray = [5,6,];
+
+  console.log(numArray.addRange(augArray));  // This will print 3,100,2,10,4,5,6
+
+}
+```
+
+ ### *removeRange()*
+___
+> Returns first elements of an array.
+```javascript
+function removeRange() {
+    let numArray = new Array<number>();
+        numArray.push(3);
+        numArray.push(100);
+        numArray.push(2);
+        numArray.push(10);
+        numArray.push(4);
+
+  console.log(numArray.removeRange(2,2));  // This will print 2,10
+
+}
+```
+### *strictSort()*
+___
+> Returns first elements of an array.
+```javascript
+function strictSort() {
+    let sArray = new Array<number>();
+        sArray.push('IronMan');
+        sArray.push('Thor');
+        sArray.push('Captain America');
+        sArray.push('Hulk');
+
+  console.log(numArray.strictSort);  
+  
+  // This will print : Captain America, Hulk, IronMan, Thor
+
+let numArray = new Array<number>();
+        numArray.push(3);
+        numArray.push(100);
+        numArray.push(2);
+        numArray.push(10);
+        numArray.push(4);
+
+ console.log(numArray.strictSort);  
+ // This will print : 2,3,4,10,100
+
+}
+```
+### *orderBy(property,sortOrder?)*
+___
+> Returns first elements of an array.
+```javascript
+function orderBy(expression,sortOrder) {
+    let sArray = new Array<number>();
+        sArray=  [{ Name: 'Tony Stark', Alias: 'Iron Man' }, { Name: 'Robert Bruce Banner', Alias: 'Hulk' },
+                  { Name: 'Steve Rojer', Alias: 'Captain America' }, { Name: 'Natasha Romanova', Alias: 'Black Widow' }];
+  
+  console.log(numArray.orderBy(item=>item.Name));  // Will sort array by Name property in Asc order
+  console.log(numArray.orderBy(item=>item.Alias));  // Will sort array by Alias property in Desc order
+}
+```
---
+++ src/extensions.ts
@@ -0,0 +1,107 @@
+import { compare } from './util';
+export { }
+declare global {
+    export interface Array<T> {
+        first(): T;
+        last(): T;
+        where(predicate: (item: T) => boolean): T[];
+        addRange(range: T[]): T[];
+        removeRange(index: number, range: number): T[];
+        strictSort(): T[];
+        orderBy(propertyExpressions: (item: T) => string, asc?: boolean): T[];
+    }
+}
+
+if (!Array.prototype.first) {
+    Array.prototype.first = function <T>(this: T[]): T {
+        if (this.length > 0) {
+            return this[0];
+        } else {
+            throw new Error("Array has no elements");
+        }
+    }
+}
+if (!Array.prototype.last) {
+    Array.prototype.last = function <T>(this: T[]): T {
+        if (this.length > 0) {
+            return this[this.length - 1];
+        } else {
+            throw new Error("Array has no elements");
+        }
+    }
+}
+
+if (!Array.prototype.where) {
+    Array.prototype.where = function <T>(this: T[], predicate: (ele: T) => boolean): T[] {
+        let resultArray = new Array<T>();
+        this.forEach((item: T) => {
+            if (predicate(item)) {
+                resultArray.push(item);
+            }
+        });
+        return resultArray
+    }
+}
+if (!Array.prototype.addRange) {
+    Array.prototype.addRange = function <T>(this: T[], listToAdd: T[]): T[] {
+        this.push(...listToAdd);
+        return this;
+    }
+}
+
+if (!Array.prototype.removeRange) {
+    Array.prototype.removeRange = function <T>(this: T[], index: number, range: number): T[] {
+        return this.splice(index, range);
+    }
+}
+
+if (!Array.prototype.strictSort) {
+    Array.prototype.strictSort = function <T>(this: T[]): T[] {
+        if (this.length > 1) {
+            const typeDetermine = typeof this[0];
+            switch (typeDetermine) {
+                case "string":
+                    return this.sort();
+                    break;
+                case "number":
+                    return this.sort(compare);
+                    break;
+                default:
+                    throw new Error("'strictSort' works with 'number' and 'string'. For sorting array of Objects, use 'orderBy' function");
+            }
+        } else {
+            throw new Error("Invalid or Insufficient items in Array");
+        }
+    }
+}
+
+if (!Array.prototype.orderBy) {
+    Array.prototype.orderBy = function <T>(this: T[], propertyExpression: (item: T) => string, asc: boolean = true): T[] {
+        if (this.length > 1) {
+            const typeDetermine = typeof this[0];
+            if (typeDetermine === "object") {
+
+                const compareFunction = (ele1: T, ele2: T): number => {
+                    for (let i = 0; i < propertyExpression.length; i++) {
+                        if (propertyExpression(ele1) > propertyExpression(ele2)) {
+                            return asc ? 1 : -1;
+                        }
+                        if (propertyExpression(ele2) > propertyExpression(ele1)) {
+                            return asc ? -1 : 1;
+                        }
+                    }
+                    return 0;
+                }
+                this.forEach((arrayItem: T) => {
+                    return this.sort(compareFunction);
+                });
+                return this;
+
+            } else {
+                throw new Error("'orderBy' works with 'object' . For sorting array of string or number, use 'strict' function");
+            }
+        } else {
+            throw new Error("Invalid or Insufficient items in Array");
+        }
+    }
+}
