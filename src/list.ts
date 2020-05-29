fc6753179283d0b2787b6cc178b1a581cc41d8c4
Mahmmad khamar shaikh <shahid13384@hotmail.com>
2020-05-29 23:13:38 +0530

    remove package-lock.json

--- src/list.ts
+++ src/list.ts
@@ -3,12 +3,19 @@ export interface IList<T> {
     add(item: T): void;
     remove(item: T): boolean;
     toList(): Array<T>;
+    addRange(range: T[]);
+    clear();
+    contains(predicate: (item: T) => boolean): boolean;
+    
 }
 
 export class List<T> implements IList<T> {
     private list: Array<T>;
     constructor() {
         this.list = new Array<T>();
+    }
+    remove(item: T): boolean {
+        throw new Error("Method not implemented.");
     }
     count = (): number => {
         return this.list.length;
